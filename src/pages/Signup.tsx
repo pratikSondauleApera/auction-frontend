71fc358efe5886f3d0e5d576c201c9fd77406b69
pratikSondauleApera <pratik@aperasoftwares.com>
2024-08-22 18:09:39 +0530

    integrated api successfully

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -23,11 +23,30 @@ function Signup() {
         const name = e.target.name;
         const value = e.target.value;
 
-        setFormData((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
+        setFormData({ ...formData, [name]: value });
     };
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault()
+
+        let url;
+
+        try {
+
+            if (userType === 'USER') {
+                url = 'http://209.182.232.11:8000/user/create-user'
+            } else {
+                url = 'http://209.182.232.11:8000/data-entry/create-user'
+            }
+
+            const res = await axios.post(url, formData)
+            const data = res.data
+            console.log('User Data ', data)
+            return data
+        } catch (error: any) {
+            console.log("ERROR ", error.response.data)
+        }
+    }
 
     async function getStates() {
         try {
@@ -67,14 +86,14 @@ function Signup() {
                 <h1>Create your account</h1>
                 <form method="POST">
                     <label>User</label>
-                    <input type="radio" value='USER' onChange={(e) => { setUserType('USER') }} />
-                    <label>Data-entry</label>
-                    <input type="radio" value='DE' onChange={(e) => { setUserType('DE') }} />
-                    <input type="text" placeholder="Firstname" value={formData.firstName} onChange={handleChange} />
-                    <input type="text" placeholder="Lastname" value={formData.lastName} onChange={handleChange} />
-                    <input type="text" placeholder="Email" value={formData.email} onChange={handleChange} />
-                    <input type="text" placeholder="Password" value={formData.password} onChange={handleChange} />
-                    <input type="text" placeholder="Phone" value={formData.phone} onChange={handleChange} />
+                    <input type="radio" name="user" value='USER' onChange={(e) => { setUserType('USER') }} />
+                    <label>Data-entry user</label>
+                    <input type="radio" name="user" value='DE' onChange={(e) => { setUserType('DE') }} />
+                    <input type="text" placeholder="Firstname" name="firstName" value={formData.firstName} onChange={handleChange} />
+                    <input type="text" placeholder="Lastname" name="lastName" value={formData.lastName} onChange={handleChange} />
+                    <input type="email" placeholder="Email" name="email" value={formData.email} onChange={handleChange} />
+                    <input type="text" placeholder="Password" name="password" value={formData.password} onChange={handleChange} />
+                    <input type="text" placeholder="Phone" name="phone" value={formData.phone} onChange={handleChange} />
                     <select name="stateId" value={formData.stateId} onChange={handleChange}>
                         <option>Select state</option>
                         {
@@ -103,10 +122,10 @@ function Signup() {
                                 : "No City Found"
                         }
                     </select>
-                    <input type="text" placeholder="Address" value={formData.address} onChange={handleChange} />
-                    <input type="text" placeholder="Pincode" value={formData.pincode} onChange={handleChange} />
+                    <input type="text" placeholder="Address" name="address" value={formData.address} onChange={handleChange} />
+                    <input type="text" placeholder="Pincode" name="pincode" value={formData.pincode} onChange={handleChange} />
 
-                    <button>Create account</button>
+                    <button onClick={handleSubmit}>Create account</button>
                 </form>
             </div>
         </>
